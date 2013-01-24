3ced09c463ed9c6a0dc0a80b295413cc58b69e9a
Jeroen <jeroen@jeroen-ubuntu.(none)>
2013-01-24 14:59:41 -0800

    added mysql user

--- debian/ohmage-server-files/install/create.aws.db.sh
+++ debian/ohmage-server-files/install/create.aws.db.sh
@@ -1,6 +1,6 @@
 #create temporary root user
 service mysql stop
-mysqld --skip-grant-tables &
+mysqld --skip-grant-tables --user=mysql &
 sleep 5s #give mysql some time to load
 mysql -u root -e "DELETE FROM mysql.user WHERE user = '';"
 mysql -u root -e "INSERT INTO mysql.user VALUES('%','awstemproot','','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','','','','',0,0,0,0,'','');"
--- debian/ohmage-server-files/install/wipe.aws.db.sh
+++ debian/ohmage-server-files/install/wipe.aws.db.sh
@@ -1,6 +1,6 @@
 #create temporary root user
 service mysql stop
-mysqld --skip-grant-tables &
+mysqld --skip-grant-tables --user=mysql &
 sleep 3s #give mysql some time to load
 mysql -u root -e "INSERT INTO mysql.user VALUES('%','awstemproot','','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','','','','',0,0,0,0,'','');"
 pkill mysql
